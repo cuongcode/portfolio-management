af3b139638295c536e4dd18c5e35b160e244d0bb
timeturnback <time.turnback@gmail.com>
2023-06-05 00:03:54 +0700

    api setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,61 +1,58 @@
 import { useState } from 'react';
+import { toast } from 'react-hot-toast';
 
 import { AddTokenForm, Board } from '@/components/pages-component/home-page';
 import { Meta } from '@/layouts/Meta';
+import { ApiInstance } from '@/services/api';
+import { handleError } from '@/services/apiHelper';
 import { Main } from '@/templates/Main';
 import coinList from '@/utils/CoinGeckoCoinsList.json';
 
 const HomePage = () => {
-  const [tokens, setTokens] = useState([]);
+  const [tokens, setTokens] = useState<any[]>([]);
 
   const onAddToken = async (symbol: string) => {
-    try {
-      // https://pro-api.coingecko.com/api/v3/
+    // https://pro-api.coingecko.com/api/v3/
 
-      // get token id from token symbol, use coinList json fetch from Coingecko
-      const coin = coinList.find((item) => item.symbol === symbol);
+    // get token id from token symbol, use coinList json fetch from Coingecko
+    const coin = coinList.find((item) => item.symbol === symbol);
 
-      const params = new URLSearchParams({
-        ids: coin.id,
-        vs_currencies: 'usd',
-        precision: '3',
-      });
+    const body = {
+      ids: coin?.id,
+      vs_currencies: 'usd',
+      precision: '3',
+    };
 
-      // fetch token price from Coingecko
-      // https://api.coingecko.com/api/v3/simple/price?
-      const response = await fetch(
-        `https://api.coingecko.com/api/v3/simple/price?${params}`
-      );
+    const res = await ApiInstance.getTokenPrice(body);
+    const { result, error } = handleError(res);
+    if (error) {
+      console.log('Something wrong in fetch token', error.message);
+      toast.error('Something wrong in fetch token');
+      return;
+    }
+    if (coin?.id) {
+      const coinPrice = result?.[coin?.id].usd;
+      const newToken = {
+        symbol: coin?.symbol,
+        name: coin?.name,
+        price: coinPrice,
+      };
 
-      if (response.status === 200) {
-        const data = await response.json();
-        const coinPrice = data[coin.id].usd;
-        const newToken = {
-          symbol: coin?.symbol,
-          name: coin?.name,
-          price: coinPrice,
-        };
-        const newTokens = [
-          ...tokens,
-          { symbol: newToken.symbol, price: newToken.price },
-        ];
-
-        setTokens(newTokens);
-      } else {
-        console.log('Something wrong in fetch token');
-      }
-    } catch (error) {
-      console.log(error);
-      alert('Please input a valid token');
+      setTokens((current) => [
+        ...current,
+        { symbol: newToken.symbol, price: newToken.price },
+      ]);
     }
   };
 
   // handle delete token
-  const tokenDeleteHandle = (deleteToken) => {
-    const newTokens = tokens.filter(
-      (token) => token.symbol !== deleteToken.symbol
-    );
-    setTokens(newTokens);
+  const tokenDeleteHandle = (deleteToken: any) => {
+    setTokens((current) => {
+      const newTokens = current.filter(
+        (token) => token.symbol !== deleteToken.symbol
+      );
+      return newTokens;
+    });
   };
 
   return (
