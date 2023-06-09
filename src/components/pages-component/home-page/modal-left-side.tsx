c1b877fe6157dadab93182bc3d3db939af0da5ab
cuongcode <congcuongnguyen.dev@gmail.com>
2023-06-09 21:07:58 +0700

    Fix bug to deply on Vercel

--- src/components/pages-component/home-page/modal-left-side.tsx
+++ src/components/pages-component/home-page/modal-left-side.tsx
@@ -1,14 +1,21 @@
-export const ModalLeftSide = ({ open, children, onClose }) => {
+import type { ReactNode } from 'react';
+
+export const ModalLeftSide = ({
+  open,
+  children,
+  onClose,
+}: {
+  open: boolean;
+  children: ReactNode;
+  onClose: () => void;
+}) => {
   if (!open) return null;
   return (
     <>
-      <div
-        className="fixed inset-0  bg-black bg-opacity-50"
-        onClick={onClose}
-      />
+      <div className="fixed inset-0  bg-black bg-opacity-50" />
       <div className="z-1 fixed left-0 top-0">
         <div className="flex w-[50vw] flex-col bg-white p-4">
-          <button className="mb-4 text-left" onClick={onClose}>
+          <button type="button" className="mb-4 text-left" onClick={onClose}>
             X
           </button>
           {children}
