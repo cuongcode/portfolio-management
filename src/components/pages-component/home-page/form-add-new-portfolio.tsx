03d3ced581b250c1f36f418fb619d45be46630b6
cuongcode <congcuongnguyen.dev@gmail.com>
2023-06-12 23:26:09 +0700

    Add new portfolion form UI

--- src/components/pages-component/home-page/form-add-new-portfolio.tsx
+++ src/components/pages-component/home-page/form-add-new-portfolio.tsx
@@ -3,15 +3,26 @@ import React from 'react';
 export const AddNewPortfolioForm = () => {
   return (
     <>
-      <h2 className="mb-4 font-bold">Seach your favorite coin</h2>
+      <h2 className="mb-4 font-bold">New Portfolio</h2>
       <div className="flex flex-col">
         <input
           className="mb-4 rounded-md border-2 p-2"
           type="text"
           id="symbol"
           name="symbol"
-          placeholder="Enter Coin Name"
+          placeholder="My Portfolio"
         />
+        <div className="flex justify-between space-x-2">
+          <button className="grow rounded-md border-2 p-2 " type="button">
+            Cancel
+          </button>
+          <button
+            className="grow rounded-md bg-green-500 p-2 text-white"
+            type="submit"
+          >
+            Create
+          </button>
+        </div>
       </div>
     </>
   );
--- src/components/pages-component/home-page/modal-center.tsx
+++ src/components/pages-component/home-page/modal-center.tsx
@@ -13,7 +13,7 @@ export const ModalCenter = ({
   return (
     // try to make animation here but fail
     <div className="fixed inset-0 flex h-screen w-screen flex-col items-center justify-center bg-black/60 transition duration-500	ease-in-out">
-      <div className="flex h-96 w-96 flex-col rounded-lg bg-white p-4">
+      <div className="flex h-fit w-96 flex-col rounded-lg bg-white p-4">
         <button type="button" className="mb-4 text-left " onClick={onClose}>
           X
         </button>
