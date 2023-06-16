b7da06816e1b51c3ac0239f55ffd6d70fb85f7ec
cuongcode <congcuongnguyen.dev@gmail.com>
2023-06-17 00:32:54 +0700

    Define type in base.ts

--- src/utils/base.ts
+++ src/utils/base.ts
@@ -1,8 +1,12 @@
-export const _sum = (array: []) => {
+export const _sum = (array: any[]) => {
   return array.reduce((sum, i) => sum + i, 0);
 };
 
-export const _zip = (array1: [], array2: [], func: (a: any, b: any) => any) => {
+export const _zip = (
+  array1: any[],
+  array2: any[],
+  func: (a: any, b: any) => any
+) => {
   return array1.map((item, index) => {
     return func(item, array2[index]);
   });
