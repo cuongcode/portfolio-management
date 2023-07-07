978174284be09397737077a845d72b915c8b69e7
ThanhHau05 <nguyenthanhhau.contact@gmail.com>
2023-07-07 18:52:41 +0700

    show no coin

--- src/components/pages/home-page/board.tsx
+++ src/components/pages/home-page/board.tsx
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { Card } from '@/components/base';
+import { CoinGeckoImages } from '@/components/images';
 import { selector, UserActions } from '@/redux';
 import { selectTotalBalance, selectTotalPNL } from '@/redux/Data/DataRedux';
 import { ApiInstance } from '@/services/api';
 import { handleError } from '@/services/apiHelper';
 
 import { AddCoinButton } from './add-coin-button';
+import { AddCoinForm } from './add-coin-form';
 import { BoardHeader } from './board-header';
 import { CoinTable } from './coin-table';
 import { ExportDataButton } from './export-data-button';
 import { ImportDataButton } from './import-data-button';
+import { ModalCenter } from './modal-center';
 
 export const Board = () => {
   const { currentUser, allUser } = useSelector(selector.user);
@@ -76,8 +79,36 @@ export const Board = () => {
         </div>
         <AddCoinButton />
       </div>
+      {currentData.length === 0 ? <CoinGeckoNoCoin /> : <CoinTable />}
+    </div>
+  );
+};
 
-      <CoinTable />
+const CoinGeckoNoCoin = () => {
+  const { currentUser } = useSelector(selector.user);
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="flex h-full flex-col items-center border-t">
+      <img
+        src={CoinGeckoImages.ImageNoCoin.src}
+        alt=""
+        className="mt-24 w-64 drop-shadow-md "
+      />
+      <h2 className="pt-3 text-lg">You don&apos;t have any favorite coins.</h2>
+      <h2
+        onClick={() => setIsOpen(true)}
+        className="cursor-pointer text-lg text-green-500 transition-all hover:text-green-700"
+      >
+        Add a new favorite coin to get started!
+      </h2>
+      <ModalCenter open={isOpen} onClose={() => setIsOpen(false)}>
+        {currentUser ? (
+          <AddCoinForm />
+        ) : (
+          <div className="text-xl">Login to add new coin.</div>
+        )}
+      </ModalCenter>
     </div>
   );
 };
