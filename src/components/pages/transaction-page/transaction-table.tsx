dcc5bd0ebebc911834213405d9330222fd0a3ee0
cuongcode <congcuongnguyen.dev@gmail.com>
2023-07-07 20:48:40 +0700

    refractor edit trans button and form

--- src/components/pages/transaction-page/transaction-table.tsx
+++ src/components/pages/transaction-page/transaction-table.tsx
@@ -1,9 +1,10 @@
 import { PencilIcon } from '@heroicons/react/outline';
+import { useState } from 'react';
 
 import type { Coin } from '@/types/Coin';
 import type { Transaction } from '@/types/Transaction';
 
-import { ButtonCenterModal } from '../home-page';
+import { ModalCenter } from '../home-page';
 import { DeleteTransactionButton } from './delete-transaction-button';
 import { EditTransactionForm } from './form-edit-transaction';
 
@@ -60,19 +61,12 @@ export const TransactionsTable = ({
             </td>
             <td>{transaction.notes}</td>
             <td className="flex items-center space-x-3">
-              <ButtonCenterModal
-                className=""
-                modalContent={
-                  <EditTransactionForm
-                    coin={coin}
-                    transaction={transaction}
-                    holdings={holdings}
-                    avgNetCost={avgNetCost}
-                  />
-                }
-              >
-                <PencilIcon className="w-4" />
-              </ButtonCenterModal>
+              <EditTransactionButton
+                coin={coin}
+                transaction={transaction}
+                holdings={holdings}
+                avgNetCost={avgNetCost}
+              />
 
               <DeleteTransactionButton coin={coin} transaction={transaction} />
             </td>
@@ -80,5 +74,35 @@ export const TransactionsTable = ({
         ))}
       </tbody>
     </table>
+  );
+};
+
+export const EditTransactionButton = ({
+  coin,
+  transaction,
+  holdings,
+  avgNetCost,
+}: {
+  coin: any;
+  transaction: any;
+  holdings: any;
+  avgNetCost: any;
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <button type="button" onClick={() => setIsOpen(true)}>
+        <PencilIcon className="w-4" />
+      </button>
+      <ModalCenter open={isOpen} onClose={() => setIsOpen(false)}>
+        <EditTransactionForm
+          coin={coin}
+          transaction={transaction}
+          holdings={holdings}
+          avgNetCost={avgNetCost}
+          onClose={() => setIsOpen(false)}
+        />
+      </ModalCenter>
+    </div>
   );
 };
--- src/utils/static-data.ts
+++ src/utils/static-data.ts
@@ -1,4 +1,4 @@
-export const staticData = [
+export const STATIC_DATA = [
   {
     id: 'bitcoin',
     symbol: 'btc',
