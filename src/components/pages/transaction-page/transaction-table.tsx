2d364eb4c4b9976c1fb11f3257f5ac9af4d36123
cuongcode <congcuongnguyen.dev@gmail.com>
2023-07-07 21:08:04 +0700

    refractor edit transaction button

--- src/components/pages/transaction-page/transaction-table.tsx
+++ src/components/pages/transaction-page/transaction-table.tsx
@@ -1,12 +1,8 @@
-import { PencilIcon } from '@heroicons/react/outline';
-import { useState } from 'react';
-
 import type { Coin } from '@/types/Coin';
 import type { Transaction } from '@/types/Transaction';
 
-import { ModalCenter } from '../home-page';
 import { DeleteTransactionButton } from './delete-transaction-button';
-import { EditTransactionForm } from './form-edit-transaction';
+import { EditTransactionButton } from './edit-transaction-button';
 
 export const TransactionsTable = ({
   transactions,
@@ -74,35 +70,5 @@ export const TransactionsTable = ({
         ))}
       </tbody>
     </table>
-  );
-};
-
-export const EditTransactionButton = ({
-  coin,
-  transaction,
-  holdings,
-  avgNetCost,
-}: {
-  coin: any;
-  transaction: any;
-  holdings: any;
-  avgNetCost: any;
-}) => {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div>
-      <button type="button" onClick={() => setIsOpen(true)}>
-        <PencilIcon className="w-4" />
-      </button>
-      <ModalCenter open={isOpen} onClose={() => setIsOpen(false)}>
-        <EditTransactionForm
-          coin={coin}
-          transaction={transaction}
-          holdings={holdings}
-          avgNetCost={avgNetCost}
-          onClose={() => setIsOpen(false)}
-        />
-      </ModalCenter>
-    </div>
   );
 };
