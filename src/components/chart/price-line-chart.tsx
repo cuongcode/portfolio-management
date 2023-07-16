c6219ea94d76db306c24c9aa7a2ccfef21a2a46b
cuongcode <congcuongnguyen.dev@gmail.com>
2023-07-16 21:25:09 +0700

    price line chart, coin option dropdown

--- src/components/chart/price-line-chart.tsx
+++ src/components/chart/price-line-chart.tsx
@@ -2,8 +2,9 @@ import { ChevronDownIcon } from '@heroicons/react/outline';
 import type { ChartItem, ChartTypeRegistry } from 'chart.js/auto';
 import { Chart } from 'chart.js/auto';
 import dayjs from 'dayjs';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
+import { useCheckClickOutside } from '@/custom-hooks/use-check-click-outside';
 import { STATIC_PRICE_DATA } from '@/utils/static-price-data';
 
 export const PriceLineChart = () => {
@@ -54,20 +55,34 @@ export const PriceLineChart = () => {
 
   return (
     <div className="m-auto flex w-full flex-col">
-      <div className="relative flex h-7 w-fit items-center gap-1 place-self-end rounded-md border-2 px-2 text-xs">
-        <div className=" font-semibold">Bitcoin</div>
-        <ChevronDownIcon className="w-3" />
-        {false ? (
-          <div className="absolute -right-1 top-7 flex w-28 flex-col rounded-lg border-2 bg-white p-1">
-            <div className="hover:rounded-sm hover:bg-[#d1e1fb]">Ethereum</div>
-            <div className="hover:rounded-sm hover:bg-[#d1e1fb]">Cardano</div>
-            <div className="hover:rounded-sm hover:bg-[#d1e1fb]">BNB</div>
-          </div>
-        ) : null}
-      </div>
+      <CoinOptionDropdown />
       <div className="">
         <canvas id="myChart" />
       </div>
     </div>
+  );
+};
+
+const CoinOptionDropdown = () => {
+  const [isDropdown, setIsDropdown] = useState(false);
+  const outsideRef = useCheckClickOutside(() => setIsDropdown(false));
+
+  return (
+    <button
+      ref={outsideRef}
+      type="button"
+      onClick={() => setIsDropdown(true)}
+      className="relative flex h-7 w-fit items-center gap-1 place-self-end rounded-md border-2 px-2 text-xs"
+    >
+      <div className="font-semibold">Bitcoin</div>
+      <ChevronDownIcon className="w-3" />
+      {isDropdown ? (
+        <div className="absolute -right-1 top-7 flex w-28 flex-col rounded-lg border-2 bg-white p-1">
+          <div className="hover:rounded-sm hover:bg-[#d1e1fb]">Ethereum</div>
+          <div className="hover:rounded-sm hover:bg-[#d1e1fb]">Cardano</div>
+          <div className="hover:rounded-sm hover:bg-[#d1e1fb]">BNB</div>
+        </div>
+      ) : null}
+    </button>
   );
 };
